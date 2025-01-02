b23f2affe291ea7f31e7600332a4871bf7ad643a
ShreyashMs <shreyashmahalle010@gmail.com>
2025-01-03 01:34:04 +0530

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,jsx}",
-    "./components/**/*.{js,jsx}",
-    "./app/**/*.{js,jsx}",
-    "./src/**/*.{js,jsx}",
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
   ],
   prefix: "",
   theme: {
@@ -74,4 +74,4 @@ export default {
     },
   },
   plugins: [require("tailwindcss-animate")],
-};
+}
